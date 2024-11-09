9be026e76952e24a9dd89f7e6da9fae74352ed28
jriddle11 <riddlejosh11@gmail.com>
2024-11-09 15:36:04 -0600

    remove css file

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -9,8 +9,6 @@
 </template>
 
 <script>
-import '@/styles/page.css'
-
 import screenSizeMixin from '@/mixins/screenSizeMixin';
 
 export default {
@@ -24,6 +22,34 @@ export default {
 </script>
 
 <style scoped>
+.side-stripe {
+    position: absolute; /* Position absolutely within the parent */
+    top: 0;            /* Align to top */
+    bottom: 0;         /* Stretch to bottom */
+    width: 5%;       /* Width of the stripe */
+    background: repeating-linear-gradient(
+      135deg,
+      rgb(155, 155, 155) 5px,
+      rgb(155, 155, 155) 5px,
+      transparent 10px,
+      transparent 50px
+    );
+  }
+  
+  .left {
+    left: 0;           /* Position on the left side */
+  }
+  
+  .right {
+    right: 0;          /* Position on the right side */
+  }
+
+  .page {
+    min-height: 70vh;
+    align-items: center;
+    background-color: #ffffff;
+    position: relative;
+  }
 .faq {
   display: flex;
   flex-direction: column;
--- src/views/FAQView.vue
+++ src/views/FAQView.vue
@@ -16,7 +16,6 @@
 </template>
 
 <script>
-import '@/styles/page.css'
 
 import Accordion from 'primevue/accordion';
 import AccordionPanel from 'primevue/accordionpanel';
@@ -46,6 +45,34 @@ export default {
 </script>
 
 <style scoped>
+.side-stripe {
+    position: absolute; /* Position absolutely within the parent */
+    top: 0;            /* Align to top */
+    bottom: 0;         /* Stretch to bottom */
+    width: 5%;       /* Width of the stripe */
+    background: repeating-linear-gradient(
+      135deg,
+      rgb(155, 155, 155) 5px,
+      rgb(155, 155, 155) 5px,
+      transparent 10px,
+      transparent 50px
+    );
+  }
+  
+  .left {
+    left: 0;           /* Position on the left side */
+  }
+  
+  .right {
+    right: 0;          /* Position on the right side */
+  }
+
+  .page {
+    min-height: 70vh;
+    align-items: center;
+    background-color: #ffffff;
+    position: relative;
+  }
 .faq {
   display: flex;
   flex-direction: column;
